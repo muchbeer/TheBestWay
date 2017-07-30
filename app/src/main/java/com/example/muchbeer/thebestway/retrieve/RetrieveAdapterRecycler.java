06bc0e7332cd52c1688713dc6aa865deb630d033
muchbeer <gman2them@gmail.com>
2017-07-30 17:05:42 +0100

    Aioui is now working perfectly

--- app/src/main/java/com/example/muchbeer/thebestway/retrieve/RetrieveAdapterRecycler.java
+++ app/src/main/java/com/example/muchbeer/thebestway/retrieve/RetrieveAdapterRecycler.java
@@ -1,34 +1,42 @@
 package com.example.muchbeer.thebestway.retrieve;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
 import com.example.muchbeer.thebestway.AppController;
 import com.example.muchbeer.thebestway.ItemPojo;
 import com.example.muchbeer.thebestway.R;
-import com.example.muchbeer.thebestway.util.NetworkController;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+
+import android.os.Handler;
+
+import static com.example.muchbeer.thebestway.R.id.itemName;
+
 
 /**
  * Created by muchbeer on 26/07/2017.
  */
 
+/**
+ * RecyclerView adapter enabling undo on a swiped away item.
+ */
+
 public class RetrieveAdapterRecycler extends RecyclerView.Adapter<RetrieveAdapterRecycler.ViewHolderRecycler> {
 
-
-     Context context;
+    private static final String LOG = RetrieveAdapterRecycler.class.getSimpleName();
+    Context context;
     ImageLoader imageLoader = AppController.getInstance().getImageLoader();
     private LayoutInflater inflater;
 
@@ -36,12 +44,25 @@ public class RetrieveAdapterRecycler extends RecyclerView.Adapter<RetrieveAdapte
     //List of superHeroes
     ArrayList<ItemPojo> itemProductList;
 
+    //List of item to be deleted after swipe undo
+    ArrayList<ItemPojo> itemsPendingRemoval =   new ArrayList<>();;
+    HashMap<ItemPojo, Runnable> pendingRunnables = new HashMap<>(); // map of items to pending runnables, so we can cancel a removal if need be
+
+
+    //Swiping detail declaration
+    int lastInsertedIndex; // so we can add some more items for testing purposes
+    private static final int PENDING_REMOVAL_TIMEOUT = 4000; // 4sec
+    boolean undoOn; // is undo on, you can turn it on from the toolbar menu
+    private Handler handler = new Handler(); // hanlder for running delayed runnables
+
+
     public RetrieveAdapterRecycler(Context context, ArrayList<ItemPojo> itemProduct, CustomItemClickListener listener){
         super();
         //Getting all the superheroes
         this.itemProductList = itemProduct;
         this.context = context;
         this.clickListener = listener;
+        itemsPendingRemoval = new ArrayList<>();
 
     }
 
@@ -64,25 +85,54 @@ public class RetrieveAdapterRecycler extends RecyclerView.Adapter<RetrieveAdapte
 
     @Override
     public void onBindViewHolder(RetrieveAdapterRecycler.ViewHolderRecycler holder, int position) {
-        Context mContext = holder.thumbNail.getContext();
 
-        ItemPojo itemPjo = itemProductList.get(position);
+             ItemPojo itemPjo = itemProductList.get(position);
+
+        final ItemPojo itemNameTobeDeleted = itemProductList.get(position);
        // imageLoader.get(superHero.getImageUrl(), ImageLoader.getImageListener(holder.imageView, R.mipmap.ic_launcher, android.R.drawable.ic_dialog_alert));
+         if (itemsPendingRemoval.contains(itemProductList)) {
+            // we need to show the "undo" state of the row
+            holder.itemView.setBackgroundColor(Color.RED);
+            holder.thumbNail.setVisibility(View.GONE);
+            holder.itemName.setVisibility(View.GONE);
+            holder.itemLocation.setVisibility(View.GONE);
+            holder.itemPrice.setVisibility(View.GONE);
+            holder.undoButton.setVisibility(View.VISIBLE);
 
 
-        holder.itemName.setText(itemPjo.getTitle());
-        holder.itemLocation.setText(itemPjo.getLocation());
-        holder.itemPrice.setText(itemPjo.getPrice());
-String urlString = itemProductList.get(position).getThumbnailUrl();
+            holder.undoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        afterDeleteClicked(itemNameTobeDeleted);
+                }
+            });
+        } else {
+            // we need to show the "normal" state
+             holder.itemView.setBackgroundColor(Color.WHITE);
+             holder.thumbNail.setVisibility(View.VISIBLE);
+             holder.itemName.setVisibility(View.VISIBLE);
+             holder.itemLocation.setVisibility(View.VISIBLE);
+             holder.itemPrice.setVisibility(View.VISIBLE);
+             holder.undoButton.setVisibility(View.GONE);
+             holder.undoButton.setOnClickListener(null);
+
+             //The rest will be the normal routine
+
+             holder.itemName.setText(itemPjo.getTitle());
+             holder.itemLocation.setText(itemPjo.getLocation());
+             holder.itemPrice.setText(itemPjo.getPrice());
+             String urlString = itemProductList.get(position).getThumbnailUrl();
 
 
-        urlString.replace("http", "https");
-        String urlString2 =  "http://api.learn2crack.com/android/images/donut.png";
-        Log.i("IMAGEPICASSO ", urlString.replace("http", "https"));
-      //  vh.picture.setDefaultImageResId(R.drawable.recycler);
-        RetrieveImage downloadVolleyImage = new RetrieveImage();
-      //  downloadVolleyImage.getImage(urlString.replace("http", "https"), holder.thumbNail);
-       PicassoAio.downloadImage(context, urlString.replace("http", "https"), holder.thumbNail);
+             urlString.replace("http", "https");
+             String urlString2 =  "http://api.learn2crack.com/android/images/donut.png";
+             Log.i("IMAGEPICASSO ", urlString.replace("http", "https"));
+             //  vh.picture.setDefaultImageResId(R.drawable.recycler);
+             RetrieveImage downloadVolleyImage = new RetrieveImage();
+             //  downloadVolleyImage.getImage(urlString.replace("http", "https"), holder.thumbNail);
+             PicassoAio.downloadImage(context, urlString.replace("http", "https"), holder.thumbNail);
+         }
+
 
 
       //  Picasso.with(activity).load(url).transform(new CircleTransform()).into(imageView);
@@ -96,21 +146,87 @@ String urlString = itemProductList.get(position).getThumbnailUrl();
                     fit().
                     centerCrop().
                     into(holder.thumbNail);*/
-        }/* else {
+        }
+
+
+        /* else {
             holder.thumbNail.setImageResource(R.drawable.recycler);
         }
 */
 
+
+        private void afterDeleteClicked  (ItemPojo title_name) {
+
+            // user wants to undo the removal, let's cancel the pending task
+            Runnable pendingRemovalRunnable = pendingRunnables.get(title_name);
+            pendingRunnables.remove(title_name);
+            if (pendingRemovalRunnable != null) handler.removeCallbacks(pendingRemovalRunnable);
+            itemsPendingRemoval.remove(title_name);
+            // this will rebind the row in "normal" state
+            notifyItemChanged(itemProductList.indexOf(title_name));
+        }
     @Override
     public int getItemCount() {
         return itemProductList.size();
     }
 
+    public void setUndoOn(boolean undoOn) {
+        this.undoOn = undoOn;
+    }
+
+    public boolean isUndoOn() {
+        return undoOn;
+    }
+
+    public void pendingRemoval(int position) {
+        //make note here instead of ItemName
+        final ItemPojo itemNamePojo = itemProductList.get(position);
+
+        if (!itemsPendingRemoval.contains(itemNamePojo)) {
+            itemsPendingRemoval.add(itemNamePojo);
+            // this will redraw row in "undo" state
+            notifyItemChanged(position);
+            // let's create, store and post a runnable to remove the item
+            Runnable pendingRemovalRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    remove(itemProductList.indexOf(itemNamePojo));
+                }
+            };
+            handler.postDelayed(pendingRemovalRunnable, PENDING_REMOVAL_TIMEOUT);
+            pendingRunnables.put(itemNamePojo, pendingRemovalRunnable);
+        }
+    }
+
+    public void remove(int position) {
+
+
+        ItemPojo itemNamePojeRemove = itemProductList.get(position);
+        if (itemsPendingRemoval.contains(itemNamePojeRemove)) {
+         //   itemsPendingRemoval.remove(itemName);
+            Toast.makeText(context, "Items PendingRemoval contain " + itemNamePojeRemove, Toast.LENGTH_LONG).show();
+            Log.i(LOG, "Items PendingRemoval contain " + itemName);
+
+
+        }
+        if (itemProductList.contains(itemName)) {
+           // itemProductList.remove(position);
+            //Do the remove here from the database
+          // notifyItemRemoved(position);
+            Toast.makeText(context, "Now your item will be removed for good", Toast.LENGTH_LONG).show();
+            Log.i(LOG, "Your item will be removed for goods");
+        }
+    }
+
+    public boolean isPendingRemoval(int position) {
+        String itemName = itemProductList.get(position).getTitle();
+        return itemsPendingRemoval.contains(itemName);
+    }
     public class ViewHolderRecycler extends RecyclerView.ViewHolder {
 
         public TextView itemName, itemLocation, itemPrice;
         // private NetworkImageView imageview;
-
+        Button undoButton;
         ImageView thumbNail;
 
         public ViewHolderRecycler(View view) {
@@ -121,6 +237,7 @@ String urlString = itemProductList.get(position).getThumbnailUrl();
             itemPrice = (TextView) view.findViewById(R.id.price);
             thumbNail  = (ImageView) view.findViewById(R.id.thumbnail);
 
+            undoButton = (Button) itemView.findViewById(R.id.undo_button);
         }
 
     }
--- app/src/main/java/com/example/muchbeer/thebestway/retrieve/RetrieveImage.java
+++ app/src/main/java/com/example/muchbeer/thebestway/retrieve/RetrieveImage.java
@@ -3,8 +3,14 @@ package com.example.muchbeer.thebestway.retrieve;
 import android.app.ProgressDialog;
 import android.content.ContextWrapper;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Movie;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -12,6 +18,7 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -53,7 +60,7 @@ import static android.R.attr.data;
 public class RetrieveImage extends AppCompatActivity {
 
 
-    private static final String TAG = "RetrieveImage debug" ;
+    private static final String TAG = "RetrieveImage debug " ;
     private ProgressDialog pDialog;
     private RetrieveAdapterRecycler mAdapter;
     private RecyclerView recyclerView;
@@ -106,7 +113,7 @@ public class RetrieveImage extends AppCompatActivity {
 
             }
         });
-        recyclerView.setAdapter(mAdapter);
+
 
 
         //Handle delete onlong press
@@ -143,7 +150,48 @@ public class RetrieveImage extends AppCompatActivity {
             e.printStackTrace();
         }
 
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        recyclerView.setAdapter(mAdapter);
+
+        setSwipeForRecyclerView();
+    }
+
+    private void setSwipeForRecyclerView() {
+
+        SwipeUtil swipeHelper = new SwipeUtil(0, ItemTouchHelper.LEFT, getApplication()) {
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                RetrieveAdapterRecycler adapter = (RetrieveAdapterRecycler) recyclerView.getAdapter();
+                adapter.pendingRemoval(swipedPosition);
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                RetrieveAdapterRecycler adapter = (RetrieveAdapterRecycler) recyclerView.getAdapter();
+                if (adapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+        };
+
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(swipeHelper);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+
+        //set swipe label
+        swipeHelper.setLeftSwipeLable("Archive");
+        //set swipe background-Color
+        swipeHelper.setLeftcolorCode( ContextCompat.getColor(RetrieveImage.this, R.color.bg_screen1));
+
+    }
+
 
 
 
@@ -328,5 +376,180 @@ public Bitmap downloadImage() {
     private void showDialog() {
         if (!pDialog.isShowing())
             pDialog.show();
+    }
+
+    /**
+     * This is the standard support library way of implementing "swipe to delete" feature. You can do custom drawing in onChildDraw method
+     * but whatever you draw will disappear once the swipe is over, and while the items are animating to their new position the recycler view
+     * background will be visible. That is rarely an desired effect.
+     */
+    private void setUpItemTouchHelper() {
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // we want to cache these and not allocate anything repeatedly in the onChildDraw method
+            Drawable background;
+            Drawable xMark;
+            int xMarkMargin;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                xMark = ContextCompat.getDrawable(RetrieveImage.this, R.drawable.ic_clear_24dp);
+                xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                xMarkMargin = (int) RetrieveImage.this.getResources().getDimension(R.dimen.ic_clear_margin);
+                initiated = true;
+            }
+
+            // not important, we don't want drag & drop
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                RetrieveAdapterRecycler removeAdapter = (RetrieveAdapterRecycler)recyclerView.getAdapter();
+                if (removeAdapter.isUndoOn() && removeAdapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                RetrieveAdapterRecycler adapter = (RetrieveAdapterRecycler
+                        )recyclerView.getAdapter();
+                boolean undoOn = adapter.isUndoOn();
+                if (undoOn) {
+                    adapter.pendingRemoval(swipedPosition);
+                } else {
+                    adapter.remove(swipedPosition);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                // draw x mark
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = xMark.getIntrinsicWidth();
+                int intrinsicHeight = xMark.getIntrinsicWidth();
+
+                int xMarkLeft = itemView.getRight() - xMarkMargin - intrinsicWidth;
+                int xMarkRight = itemView.getRight() - xMarkMargin;
+                int xMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight)/2;
+                int xMarkBottom = xMarkTop + intrinsicHeight;
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom);
+
+                xMark.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    /**
+     * We're gonna setup another ItemDecorator that will draw the red background in the empty space while the items are animating to thier new positions
+     * after an item is removed.
+     */
+    private void setUpAnimationDecoratorHelper() {
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            // we want to cache this and not allocate anything repeatedly in the onDraw method
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated) {
+                    init();
+                }
+
+                // only if animation is in progress
+                if (parent.getItemAnimator().isRunning()) {
+
+                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
+                    // this is not exclusive, both movement can be happening at the same time
+                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
+                    // then remove one from the middle
+
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    // this is fixed
+                    int left = 0;
+                    int right = parent.getWidth();
+
+                    // this we need to find out
+                    int top = 0;
+                    int bottom = 0;
+
+                    // find relevant translating views
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for (int i = 0; i < childCount; i++) {
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            // view is coming down
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            // view is coming up
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else if (firstViewComingUp != null) {
+                        // views are coming up to fill the void
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+
+                }
+                super.onDraw(c, parent, state);
+            }
+
+        });
     }
 }
